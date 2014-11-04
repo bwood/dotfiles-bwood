35382915ec15b92eb3685aaa7098dd14239ae786
Brian Wood <bwood@berkeley.edu>
2014-11-04 10:29:18 -0800

    Don't copy .gitignore

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+#cd "$(dirname "${BASH_SOURCE}")";
+cd ~/bwood/code/dotfiles-bwood
 
 git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude ".gitignore" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
